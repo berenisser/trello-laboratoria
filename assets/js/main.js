ff12985be6c37474ab7504640062035098a2952f
berenisser <berenisser@gmail.com>
2017-05-17 16:32:16 -0400

    First button and input area created

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+var mainInput = document.getElementById("primerInput");
+mainInput.addEventListener("click", function(){
+    var mainBoton = document.getElementById('boton-guardar');
+    if (mainBoton.style.display === 'none') {
+        mainBoton.style.display = 'block';
+    } 
+});
+
+
