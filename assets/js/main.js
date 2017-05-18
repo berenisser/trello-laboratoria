18ac67c37a810a36a6f379d83a5c94f953a942fc
berenisser <berenisser@gmail.com>
2017-05-18 01:26:21 -0400

    Tarjetas agregadas, añadir tarjetas aparece despues dichas tarjetas

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,42 +12,39 @@ mainBoton.addEventListener("click", guardarLista);
 //Funcion que crea Lista despues de ser clickeado
 
 function guardarLista(){
+	var mainInput = document.getElementById("primerInput").value;
+	document.getElementById('primerInput').value = "";
+
 	var espacioLista = document.querySelector(".nueva-lista");
 	var nuevoInputLista = document.createElement("div");
 	nuevoInputLista.classList.add("list-content");
 	var divHeader = document.createElement("div");
 	divHeader.classList.add("list-header");
-	var listaHeader = document.createElement("h2");
-	var h2Text = document.createTextNode(mainInput.value);
+	var listaHeader = document.createElement("h4");
+	var h4Text = document.createTextNode(mainInput);
+	var divEnlace = document.createElement("div");
 	var enlaceAgregarTarjeta = document.createElement("a");
 	var textoAgregarTarjeta = document.createTextNode("Añadir una tarjeta...");
 
 	espacioLista.appendChild(nuevoInputLista);
 	nuevoInputLista.appendChild(divHeader);
 	divHeader.appendChild(listaHeader);
-	listaHeader.appendChild(h2Text);
-	divHeader.appendChild(enlaceAgregarTarjeta);
+	listaHeader.appendChild(h4Text);
+	nuevoInputLista.appendChild(divEnlace);
+	divEnlace.appendChild(enlaceAgregarTarjeta);
 	enlaceAgregarTarjeta.appendChild(textoAgregarTarjeta);
+
+	enlaceAgregarTarjeta.addEventListener('click', function(){
+		var divCajaTexto = document.createElement("div");
+	   	var cajaTexto = document.createElement('textarea');
+			
+		nuevoInputLista.appendChild(divCajaTexto);
+		divCajaTexto.appendChild(cajaTexto);
+		divCajaTexto.appendChild(divEnlace);
+		
+	});
+
+	//Falta poder presionar enter para que la funcion del boton se ejecute
+	//Faltan mas detalles de funcionalidad pero esto es lo que pude lograr
+
 }
-
-
-
-
-
-
-
-
-
-
-
-/*
-mainInput.addEventListener("click", function(){
-    if (mainBoton.style.display === 'none') {
-        mainBoton.style.display = 'block';
-    } else {
-        mainBoton.style.display = 'none';
-    }
-});
-*/
-
-
